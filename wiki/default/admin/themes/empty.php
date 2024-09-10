e2e22acedb05c50b3516eac28427b790a6c0e903
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2024-09-10 11:15:50 +0200

    remove

--- wiki/default/admin/themes/empty.php
+++
@@ -1,9 +0,0 @@
-<?php
-    use fruithost\Localization\I18N;
-	use fruithost\UI\Icon;
-?>
-<div class="jumbotron text-center bg-transparent text-muted">
-	<?php Icon::show('smiley-bad'); ?>
-	<h2><?php I18N::__('No Themes available!'); ?></h2>
-	<p class="lead"><?php I18N::__('Please install some Themes.'); ?></p>
-</div>
--- wiki/libraries/skoerfgen/ACMECert.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require __DIR__.'/src/ACME_Exception.php';
-require __DIR__.'/src/ACMEv2.php';
-require __DIR__.'/src/ACMECert.php';
